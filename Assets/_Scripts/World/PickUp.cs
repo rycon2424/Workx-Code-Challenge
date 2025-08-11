5e520e24c99fa39e4796e0674d89db869d602c8a
rycon2424 <36225966+rycon2424@users.noreply.github.com>
2025-08-11 14:18:16 +0200

    Added Reload inspector button to refresh the PickUp in runtime

--- Assets/_Scripts/World/PickUp.cs
+++ Assets/_Scripts/World/PickUp.cs
@@ -24,6 +24,7 @@ namespace Challenge.World.Interactables
             Setup();
         }
 
+        [Button("Reload")]
         private void Setup()
         {
             itemSprite.sprite = itemInfo.GetItemArt();
